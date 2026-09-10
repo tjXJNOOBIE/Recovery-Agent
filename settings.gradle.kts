29a72d57c828c62831dd8ef0df53195d85a9fd96
TJ <tjxjnoobie@gmail.com>
2026-09-10 11:56:04 -0700

    build: establish Java-first Recovery Gradle root

---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        mavenLocal()
+        gradlePluginPortal()
+    }
+}
+
+rootProject.name = "recovery-agent"
+include(":state-authority")
+
+val functionCatalogCheckout = file("../function-catalog")
+if (functionCatalogCheckout.resolve("settings.gradle.kts").isFile) {
+    includeBuild(functionCatalogCheckout)
+}
+
+val tavallDiCheckout = file("../tavall-di")
+if (tavallDiCheckout.resolve("settings.gradle.kts").isFile) {
+    includeBuild(tavallDiCheckout)
+}
+
+val tavallLoggingCheckout = file("../tavall-logging")
+if (tavallLoggingCheckout.resolve("settings.gradle.kts").isFile) {
+    includeBuild(tavallLoggingCheckout)
+}
